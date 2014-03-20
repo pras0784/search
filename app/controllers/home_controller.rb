5236e80e9963e9e6f91b1a57ce9febe0a63af63e
Harvey Ico <godstrikerharvey@gmail.com>
2014-03-21 00:39:37 +0800

    Modified scraper for IMDB

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 require 'open-uri'
 class HomeController < ApplicationController
+  def index
+    @imdb = []
+    search_imdb(params[:k])
+  end
+
 
   def suggest
     #This is for the API of getting json through the API of IMDB
@@ -11,4 +16,34 @@ class HomeController < ApplicationController
     results = JSON.parse(results.string)
     render json: results
   end
+
+  private
+
+  def search_imdb(key)
+    #mofied scraper for IMDB.com
+    key = CGI::escape key.to_s
+
+    html = Nokogiri::HTML(open("http://imdb.com/find?q=#{key}&s=all"))
+    html.search('.findSection').each do |el|
+
+      header = el.search('.findSectionHeader').text()
+      results = {
+        header => []
+      }
+
+      el.search('.findResult').each do |item|
+        img = item.at_css('.primary_photo img').attribute('src').content rescue nil
+        title = item.at_css('.result_text').text()
+        url = item.at_css('.result_text a').attribute('href').content
+        results[header] << {
+          img: img,
+          title: title,
+          url: url
+        }
+      end
+
+      @imdb << results
+    end
+  end
+
 end
